4fd964f9ab058c235f8ce029bdc0bc3a85d445a5
augustorl <gutin22@gmail.com>
2021-05-15 18:32:16 -0300

    add: theme.ts

---
+++ src/styles/themes.ts
@@ -0,0 +1,19 @@
+export const lightTheme = {
+    name: 'light',
+    colors: {
+        text: '#111517',
+        backgroundPrimary:'#fafafa',
+        backgroundSecondary: '#ffffff',
+        input: '#858585',
+    }
+}
+
+export const darkTheme = {
+    name: 'dark',
+    colors: {
+        text: '#ffffff',
+        backgroundPrimary:'#202c37',
+        backgroundSecondary: '#2b3945',
+        input: '#ffffff',
+    }
+}
