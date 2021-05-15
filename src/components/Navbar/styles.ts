1bf34f304c9bda6dc1a118c65a4aae8212122738
augustorl <gutin22@gmail.com>
2021-05-15 18:37:08 -0300

    fix: box-shadow off navbar

--- src/components/Navbar/styles.ts
+++ src/components/Navbar/styles.ts
@@ -1,6 +1,10 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+
+    header {
+        box-shadow: 0px 1px 2px -0.5px rgba(0,0,0,0.2),0px 1.5px 3px 0px rgba(0,0,0,0.14),0px 1px 5px 0px rgba(0,0,0,0.12);;
+    }
     
     div {
         display: flex;
