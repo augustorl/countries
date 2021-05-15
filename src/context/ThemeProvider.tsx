6a4c75433c4691cc2347895acfb0830f1abf4c8e
augustorl <gutin22@gmail.com>
2021-05-15 18:32:53 -0300

    add: ThemeProvider

--- src/context/ThemeProvider.tsx
+++ src/context/ThemeProvider.tsx
@@ -1,14 +1,39 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useCallback, useContext, useState } from "react";
+import { ThemeProvider } from 'styled-components';
+import { lightTheme, darkTheme } from '../styles/themes';
 
-export const ThemeContext = createContext();
-
-export function ThemeProvider({children}) {
-  const [theme, setTheme] = useState('light');
-    
-
-    return (
-      <ThemeContext.Provider value={{theme, setTheme}}>
-        {children}
-      </ThemeContext.Provider>
-    )
+interface Theme {
+  name: string;
+  colors: {
+    text: string,
+    backgroundPrimary: string,
+    backgroundSecondary: string,
+    input: string,
   }
+}
+interface ThemeContextData { theme: Theme; toggleTheme: () => void };
+
+export const ThemeContext = createContext<ThemeContextData>(
+  {} as ThemeContextData
+);
+export const useTheme = () => useContext(ThemeContext);
+
+export const CustomThemeProvider: React.FC = ({ children }) => {
+  const [theme, setTheme] = useState<Theme>(lightTheme);
+  const toggleTheme = useCallback(() => {
+    if (theme.name === 'light'){
+      setTheme(darkTheme);
+    }
+    else if (theme.name === 'dark') {
+      setTheme(lightTheme);
+    }
+  }, [theme]);
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <ThemeProvider theme={theme}>
+        {children}
+      </ThemeProvider>
+    </ThemeContext.Provider>
+  );
+};
