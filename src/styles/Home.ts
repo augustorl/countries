fcbb0df16c5158924f34b548c5fec9bdc95a3323
augustorl <gutin22@gmail.com>
2021-05-17 19:09:12 -0300

    add: Home styling

---
+++ src/styles/Home.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    margin: 0 auto;
+    height: 100vh;
+    
+
+    .search-container {
+        margin-top: 4rem;
+        display: flex;
+        align-items: center;
+        justify-content: space-around;        
+    }
+
+`;
