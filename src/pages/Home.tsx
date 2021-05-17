7ce0ebd646a5f9dff6886c414254704468f3eff9
augustorl <gutin22@gmail.com>
2021-05-17 17:12:49 -0300

    add: SearchInput to Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,21 @@
 import Navbar from "../components/Navbar"
+import SearchInput from "../components/SearchInput";
 
 const Home: React.FC = () => {
     return (
        <>
         <Navbar/>
+        <div>
+            <SearchInput/>
+            <select  defaultValue={"default"} placeholder="">
+                <option value="default" disabled hidden>Filter by region</option>
+                <option value="africa">Africa</option>
+                <option value="america">America</option>
+                <option value="asia">Asia</option>
+                <option value="europe">Europe</option>
+                <option value="oceania">Oceania</option>
+            </select>
+        </div>
        </>
     );
   };
