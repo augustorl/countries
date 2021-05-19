1fea7807e42fe1c449d62dcdd125ec4b275fbaf9
augustorl <gutin22@gmail.com>
2021-05-19 14:29:43 -0300

    update: home styling

--- src/styles/Home.ts
+++ src/styles/Home.ts
@@ -1,14 +1,25 @@
 import styled from 'styled-components';
+import media from './mediaQueries';
 
 export const Container = styled.div`
     margin: 0 auto;
-    max-width: 1820px;
+    
+
 
     .search-container {
         margin-top: 3rem;
         display: flex;
         align-items: center;
         justify-content: space-around;
+        
+        .MuiFilledInput-underline::after,
+        .MuiFilledInput-underline::before {
+            border-bottom: none !important;
+        }
+        @media (${media.lg}) {
+            margin-top: 4.8rem;
+            margin-bottom: 4.5rem;
+        }
     }
 
     .country-list {
