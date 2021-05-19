2e3c44a4f364c5d62d8627fb1131b61d42d52d7b
augustorl <gutin22@gmail.com>
2021-05-18 22:46:18 -0300

    update: Country List to Filter Context

--- src/components/Country/CountryList/index.tsx
+++ src/components/Country/CountryList/index.tsx
@@ -1,15 +1,33 @@
 import { useContext } from 'react';
 import CountryContext from '../../../context/CountryContext';
+import FilterCountryContext from '../../../context/FilterCountryContext';
 import { CardList } from './styles';
+import CountryCard  from '../CountryCard';
 
 const CountryList: React.FC = () => {
   const { countries } = useContext(CountryContext);
+  const { name, region } = useContext(FilterCountryContext);
+
+  const filterByName = (countries: any[]) => {
+    if (name) {
+      return countries.filter((country: { name: string; }) =>
+        country.name.toLowerCase().includes(name.toLowerCase().trim())
+      );
+    } else {
+      return countries;
+    }
+  };
+  const filterByRegion = (countries: any[]) => {
+    if (region) {
+      return countries.filter((country: { region: string; }) => country.region === region);
+    } else return countries;
+  };
 
   return (
     <CardList>
-        {countries && (
-            <h1>ola</h1>
-        )}
+      {filterByName(filterByRegion(countries)).map((item: any) => (
+        <CountryCard key={item.alpha3Code} data={item} />
+      ))}
     </CardList>
   );
 };
