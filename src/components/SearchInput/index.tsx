3bb1ad840f1ca41ce1b5b4303339edbe39fd3fc3
augustorl <gutin22@gmail.com>
2021-05-18 23:25:35 -0300

    add: FilterContext to Search input

--- src/components/SearchInput/index.tsx
+++ src/components/SearchInput/index.tsx
@@ -1,16 +1,23 @@
 import { IconButton } from "@material-ui/core";
 import InputBase from "@material-ui/core/InputBase";
 import Paper from "@material-ui/core/Paper";
+import { useContext } from "react";
+import FilterCountryContext from "../../context/FilterCountryContext";
 import { Container, Icon } from "./styles";
 
 export default function SearchInput() {
+    const { name, setName } = useContext(FilterCountryContext);
+    const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setName && setName(e.target.value);
     return (
-        <Container>
+        <Container onSubmit={e => e.preventDefault}>
             <Paper component="form">
                 <IconButton type="submit" aria-label="search">
                     <Icon />
                 </IconButton>
                 <InputBase
+                    value={name}
+                    onChange={handleNameChange}
                     placeholder="Search for a country..."
                     inputProps={{ 'aria-label': 'Search for a country...' }}
                 />
