aeb5209046cf99c10ae15aeb2c68a7fcd70f5894
augustorl <gutin22@gmail.com>
2021-05-18 22:01:22 -0300

    add: mediaqueries

---
+++ src/styles/mediaQueries.ts
@@ -0,0 +1,9 @@
+const media = {
+    sm: `min-width: ${560}px`,
+    md: `min-width: ${720}px`,
+    lg: `min-width: ${1040}px`,
+    xl: `min-width: ${1440}px`,
+  };
+  
+  export default media;
+  
